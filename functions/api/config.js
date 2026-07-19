6d7548231e107a04bf5fa314e02946fd0b3fe362
josegermanepz <josegermanepz@gmail.com>
2026-07-19 08:20:51 -0600

    Actualizar disponibilidad y correo de agenda

--- functions/api/config.js
+++ functions/api/config.js
@@ -1,1 +1,7 @@
-import{json}from'../_shared.js';export const onRequestGet=async({env})=>json({publicKey:env.MP_PUBLIC_KEY||''});
+import {json} from '../_shared.js';
+
+export const onRequestGet=async({env})=>json({
+  bookingEmail:'yunuen.preg@gmail.com',
+  calendarConnected:Boolean(env.GOOGLE_CALENDAR_ICS_URL),
+  paymentsEnabled:false
+});
